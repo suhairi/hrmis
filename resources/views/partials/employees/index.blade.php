1befb824c030342accd06bb1663937592a7adc96
Suhairi <suhairi81@gmail.com>
2023-04-26 15:56:45 +0800

    20230426 002

--- resources/views/partials/employees/index.blade.php
+++ resources/views/partials/employees/index.blade.php
@@ -5,7 +5,7 @@
         hover:-translate-y-1 duration-300
         rounded-full hover:shadow-md shadow-md" 
     >
-        <i class="fa fa-eye mr-2"> </i>Papar
+        <i class="fa fa-eye mr-2"></i>
     </a>
     <a href="{{ route('employees.edit', $employee->id) }}"
         class="btn button bg-green-400 hover:bg-green-500 text-white hover:font-semibold
@@ -13,10 +13,10 @@
         hover:-translate-y-1 duration-300
         rounded-full hover:shadow-md shadow-md" 
     >
-        <i class="fa fa-pencil-square mr-2"> </i>Kemaskini
+        <i class="fa fa-pencil-square mr-2"></i>
     </a>
     {!! Form::open(['method' => 'DELETE','route' => ['employees.destroy', $employee->id],'style'=>'display:inline']) !!}
-        {{ Form::button('<i class="fa fa-trash mr-2"></i>Hapus', ['type' => 'submit', 
+        {{ Form::button('<i class="fa fa-trash mr-2"></i>', ['type' => 'submit', 
                 'class' => 'btn button bg-red-600 hover:bg-red-700 text-white hover:font-semibold
                 transition ease-in-out delay-30
                 hover:-translate-y-1 duration-300
