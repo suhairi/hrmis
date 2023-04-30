580b5af8e277b6f5096e5a86f125b1c29621bde9
Suhairi <suhairi81@gmail.com>
2023-04-30 12:01:49 +0800

    20230430 003

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -42,12 +42,12 @@
                                 Pengurusan Prestasi</span></a>
                     </li>
 
-                    @if(Str::contains(Auth::user()->location, 'WILAYAH'))
+                    <!-- @if(Str::contains(Auth::user()->location, 'WILAYAH')) -->
                         <li>
                             <a href="{{ route('directory.index') }}"><img src="{{ URL::to('assets/img/performance.svg') }}" alt="sidebar_img"><span>
                                     Direktori PPK</span></a>
                         </li>
-                    @endif
+                    <!-- @endif -->
 
                     @if(Str::contains(Auth::user()->location, 'HQ'))
                         <li>
